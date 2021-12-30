e1910b4c24d1ae02b52b5d1cb22d0b6a6a9f6cb8
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-12-31 02:36:52 +0300

    deleted

--- dist/calculator.js
+++
@@ -1,1 +0,0 @@
-function calculator(n,u,t){let r=u;for(const o of t)r=n(r,o);return r}function sum(n,u){return n+u}function minus(n,u){return n-u}function multiply(n,u){return n*u}function divider(n,u){return n/u}result=calculator(sum,48,[3,2]),console.log(result);
